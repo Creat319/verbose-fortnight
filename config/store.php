f3476780ade5ed063cee58b43f21c6964d99acda
Creat319 <2725023335@qq.com>
2025-05-10 20:01:28 +0800

    Add files via upload

---
+++ config/store.php
@@ -0,0 +1,7 @@
+<?php
+declare (strict_types=1);
+
+return [
+    'app_id' => '41085',
+    'app_key' => '8ab5c557fc3b2bd4b5b02b3d2c56acd4',
+];
---
+++ kernel/Install/Update/1.4.1/file/app/Model/Config.php
@@ -0,0 +1,99 @@
+<?php
+declare (strict_types=1);
+
+namespace App\Model;
+
+
+use App\Util\Context;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int $id
+ * @property string $key
+ * @property string $value
+ */
+class Config extends Model
+{
+    /**
+     * @var string
+     */
+    protected $table = 'config';
+
+    /**
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * @var array
+     */
+    protected $casts = ['id' => 'integer'];
+
+    /**
+     * @return int
+     */
+    public static function getSessionExpire(): int
+    {
+        $expire = self::get("session_expire") ?: (86400 * 30);
+        if ($expire < 120) {
+            return 86400 * 30;
+        }
+        return $expire;
+    }
+
+
+    /**
+     * 为了方便，在这里直接静态get
+     * @param string $key
+     * @return string
+     */
+    public static function get(string $key): string
+    {
+
+        $cacheKey = "_DB_CONFIG_" . $key;
+        $cache = Context::get($cacheKey);
+
+        if ($cache) {
+            return (string)$cache;
+        }
+
+        $cfg = Config::query()->where("key", $key)->first();
+        if (!$cfg) {
+            return "";
+        }
+
+        //存储
+        Context::set($cacheKey, $cfg->value);
+        return (string)$cfg->value;
+    }
+
+    /**
+     * @return array
+     */
+    public static function list(): array
+    {
+        $cfg = Config::query()->get();
+        $list = [];
+        foreach ($cfg as $item) {
+            $list[$item->key] = $item->value;
+        }
+        return $list;
+    }
+
+
+    /**
+     * @param string $key
+     * @param string|int $value
+     */
+    public static function put(string $key, string|int $value): void
+    {
+        $cfg = Config::query()->where("key", $key)->first();
+        if (!$cfg) {
+            $cfg = new Config();
+            $cfg->key = $key;
+        }
+        $cfg->value = $value;
+        $cfg->save();
+    }
+
+}
